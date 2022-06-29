ace121aa6024344d653c742daf10b3f46e70b5fc
Sarkhanas <daniilzon@mail.ru>
2022-06-29 04:51:24 +0300

    Update Wall.cs

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -6,6 +6,17 @@ public class Wall : MonoBehaviour
 {
     public GameObject block;
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Block"))
+        {
+            Debug.Log("Block behind the Wall!");
+            Instantiate(block, transform.GetChild(0).position, Quaternion.identity);
+            Instantiate(block, transform.GetChild(1).position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
     private void OnTriggerStay2D(Collider2D other)
     {        
         if (other.CompareTag("Block"))
